f874ab1d17599d6beb61125cbb6c781f0a2e656c
Dylan Pereira <perdylan@gmail.com>
2020-09-30 10:16:15 +1000

    changed console message on server startup in aws

--- index.js
+++ index.js
@@ -57,4 +57,4 @@ app.use(function (err, req, res, next) {
 
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log("the server should be listening on port 8080..."));
+app.listen(port, () => console.log("the server(/api) should be listening on port 8080..."));
